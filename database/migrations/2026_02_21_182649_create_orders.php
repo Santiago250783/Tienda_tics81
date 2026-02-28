ea8bcdbba6a109e9728359a2dcabc3969b84fc2f
sabtiago250783 <santiagosv354@gmail.com>
2026-02-28 12:25:02 -0600

    mi primer comit

---
+++ database/migrations/2026_02_21_182649_create_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration 
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("name"); //campo para nombre del producto
+            $table->text("descripcion")->nullable(); //campo para
+            $table->decimal("price, 10,2")->default(0);
+            $table->string("slug;");
+            $table->integer("stock")->default(0);
+            $table->boolean("is_active")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
